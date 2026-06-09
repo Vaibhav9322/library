d88b8c8bd8cdb4b85f32a6fa36771ad019d1f3b3
VaibhavAdhav <109598773+Vaibhav9322@users.noreply.github.com>
2026-06-09 20:07:01 +0530

    intial commit

--- src/main/java/com/library/entity/Book.java
+++ src/main/java/com/library/entity/Book.java
@@ -66,7 +66,6 @@ public class Book {
     
     public double getPrice() { return price; }
     public void setPrice(double price) { this.price = price; }
-    
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 }
